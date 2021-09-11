476df7d967efc44d5c429597341412409eaa517e
SolChanP <qkrthfcks1@naver.com>
2021-09-11 23:26:34 +0900

    [MYIAPP-5] HOST 주소 외부로 빼기

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import styled from 'styled-components'
-import { Flex, TabBar } from 'antd-mobile';
 
 ReactDOM.render(
   <React.StrictMode>
